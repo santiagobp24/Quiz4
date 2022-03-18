97c6a352e66c1bbdbbe82c1bec6205cca505db9c
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 23:45:00 -0500

    Primer

---
+++ src/test/java/Ejemplos005/Central22/AppTest.java
@@ -0,0 +1,20 @@
+package Ejemplos005.Central22;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
